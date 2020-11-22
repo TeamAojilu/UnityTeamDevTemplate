0cd77d5b4a14ec703e190bde9a70b4c2b95a8873
SilCil <65537555+SilCil@users.noreply.github.com>
2020-11-22 14:12:34 +0900

    Add: Singleton, SceneLoader
    グローバルな処理はシングルトンが便利なので作りました。とりあえず、シーンロード処理を追加。

--- UnityProject/Assets/_Assets/SampleGames/RollBall/Scripts/Play/GameManager.cs
+++ UnityProject/Assets/_Assets/SampleGames/RollBall/Scripts/Play/GameManager.cs
@@ -1,17 +1,26 @@
 ﻿using System.Collections;
 using UnityEngine;
 using SilCilSystem.Variables;
+using SilCilSystem.SceneLoader;
 
 namespace RollBall
 {
     public class GameManager : MonoBehaviour
     {
+        [Header("State")]
         [SerializeField] private VariableBool m_isPlaying = default;
         [SerializeField] private ReadonlyFloat m_time = default;
         [SerializeField] private ReadonlyInt m_itemCount = default;
+        
+        [Header("Next Scene")]
+        [SerializeField] private string m_nextScene = "Title";
+        [SerializeField] private float m_waitTimeToNextScene = 3f;
 
         public IEnumerator Start()
         {
+            // 遷移処理待ち.
+            yield return SceneLoader.WaitLoading;
+
             // ゲーム開始.
             m_isPlaying.Value = true; 
 
@@ -20,6 +29,10 @@ namespace RollBall
 
             // ゲーム終了.
             m_isPlaying.Value = false;
+
+            // しばらく待機してから次のシーンへ.
+            yield return new WaitForSeconds(m_waitTimeToNextScene);
+            SceneLoader.LoadScene(m_nextScene);
         }
     }
 }
---
+++ UnityProject/Assets/_Assets/SilCilSystem/Scripts/_Settings/PrefabGeneratorOnLoad.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace SilCilSystem
+{
+    /// <summary>
+    /// ゲーム開始時にプレハブを自動生成するためのクラス.
+    /// シングルトンを自動生成するために作成.
+    /// Resourcesを使っているので、ビルド時は最初のシーンにプレハブを置いてしまうほうがいいと思う.
+    /// </summary>
+    internal static class PrefabGeneratorOnLoad
+    {
+        private const string Path = "InitialPrefabs";
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void Init()
+        {
+            foreach (var prefab in Resources.LoadAll<GameObject>(Path))
+            {
+                GameObject.Instantiate(prefab);
+            }
+        }
+    }
+}
