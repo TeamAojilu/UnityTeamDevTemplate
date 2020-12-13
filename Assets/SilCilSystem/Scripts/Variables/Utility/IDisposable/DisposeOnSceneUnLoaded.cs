7e8ddcd50a3c796e0211b15e2742c92a2855c2d9
SilCil <65537555+SilCil@users.noreply.github.com>
2020-12-13 20:25:30 +0900

    Fix: namespace of DisposeOnSceneUnLoaded
    DisposeOnSceneUnLoadedメソッドの名前空間をSilCilSystem.Variablesに変更しました。

--- Assets/SilCilSystem/Scripts/Variables/Utility/IDisposable/DisposeOnSceneUnLoaded.cs
+++ Assets/SilCilSystem/Scripts/Variables/Utility/IDisposable/DisposeOnSceneUnLoaded.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace SilCilSystem
+namespace SilCilSystem.Variables
 {
     public static class DisposeOnSceneUnLoadedExtensios
     {
