21d1497cac42a52756ca9feb1e89fa9565146a32
SilCil <65537555+SilCil@users.noreply.github.com>
2020-12-16 20:58:17 +0900

    Add: DisposeOnDestroy
    Disposeに役立つ拡張メソッドを追加

---
+++ Assets/SilCilSystem/Scripts/Variables/Utility/IDisposable/DisposeOnDestroyCaller.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace SilCilSystem.Variables
+{
+    internal class DisposeOnDestroyCaller : MonoBehaviour
+    {
+        private CompositeDisposable m_disposable = new CompositeDisposable();
+
+        private void OnDestroy()
+        {
+            m_disposable?.Dispose();
+        }
+
+        internal void Set(IDisposable disposable)
+        {
+            m_disposable.Add(disposable);
+        }
+    }
+
+    public static class DisposeOnDestroyCallerExtensions
+    {
+        /// <summary>ゲームオブジェクトが破棄される時に自動でDisposeが呼ばれるようにする</summary>
+        public static void DisposeOnDestroy(this IDisposable disposable, GameObject gameObject)
+        {
+            if (gameObject == null) return;
+
+            if(gameObject.TryGetComponent(out DisposeOnDestroyCaller caller))
+            {
+                caller.Set(disposable);
+            }
+            else
+            {
+                gameObject.AddComponent<DisposeOnDestroyCaller>().Set(disposable);
+            }
+        }
+    }
+}
