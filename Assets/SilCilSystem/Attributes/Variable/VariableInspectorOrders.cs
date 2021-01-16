2ab5c73244b85ecfa4343e2c4f6fef7ecf10fd2e
SilCil <65537555+SilCil@users.noreply.github.com>
2021-01-16 10:47:51 +0900

    Add: UNITY_EDITOR in VariableInspectroOrder
    シンボル入れ忘れでビルド通らなかったので修正

--- Assets/SilCilSystem/Attributes/Variable/VariableInspectorOrders.cs
+++ Assets/SilCilSystem/Attributes/Variable/VariableInspectorOrders.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿#if UNITY_EDITOR
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEditor;
@@ -114,3 +115,4 @@ namespace SilCilSystem.Editors
         }
     }
 }
+#endif
